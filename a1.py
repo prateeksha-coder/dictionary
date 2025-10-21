0ad2210d6cf243043f88ba63fa25d9ffbf8ec9c8
prateeksha-coder <prateeksha.codingal@gmail.com>
2025-10-21 17:36:54 +0530

    commit

--- a1.py
+++ a1.py
@@ -1,4 +1,17 @@
-student_data = {'id1': 
+student_data = {1:{'name':'sara','age':6},2:{'name':'jeff','age':7},3:{'name':'alia','age':9},4:{'name':'sara','age':6}}
+
+print(student_data.items())
+result = {}
+
+for key,value in student_data.items():
+    if value not in result.values():
+        result[key] = value
+
+print(result)
+
+
+#d1={1:{'name':'sara','age':6},2:{'name':'jeff','age':7},3:{'name':'alia','age':9}}
+'''{'id1': 
    {'name': ['Sara'], 
     'class': ['V'], 
     'subject_integration': ['english, math, science']
@@ -18,16 +31,5 @@ student_data = {'id1':
     'class': ['V'], 
     'subject_integration': ['english, math, science']
    },
-}
+}'''
 
-print(student_data.items())
-result = {}
-
-for key,value in student_data.items():
-    if value not in result.values():
-        result[key] = value
-
-print(result)
-
-
-#d1={1:{'name':'sara','age':6},2:{'name':'jeff','age':7},3:{'name':'alia','age':9}}
